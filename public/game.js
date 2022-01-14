14e14f7ac00431409ce6c1eb4065806c8c49465e
Dev <dbrhy95@gmail.com>
2022-01-13 20:10:06 -0600

    color picker, baby!

--- public/game.js
+++ public/game.js
@@ -1,4 +1,5 @@
 //this is the client file
+//import Pickr from '@simonwep/pickr';
 const socket = io();
 let roomID;
 let playerName;
@@ -123,7 +124,8 @@ brushSize.addEventListener("input", inputHandler);
 brushSize.addEventListener("change", changeHandler); //this is the preferred pattern
 var brushWidth = 5;
 brushSizeDisplay.innerHTML = "Brush Size: " + brushWidth;
-canvas.freeDrawingBrush.color = '#000000';
+var brushColor = '#000000';
+canvas.freeDrawingBrush.color = brushColor;
 canvas.freeDrawingBrush.width = brushWidth;
 
 $("#eraseMode").click(function () {
@@ -134,7 +136,6 @@ $("#eraseMode").click(function () {
 $("#drawMode").click(function () {
     canvas.freeDrawingBrush = pen;
     canvas.freeDrawingBrush.width = brushWidth;
-    canvas.freeDrawingBrush.color = '#000000';
 });
 
 function inputHandler(event) { //preferred pattern
@@ -192,3 +193,46 @@ function logToServer(value, msg) {
         message: msg
     });
 }
+
+const pickr = Pickr.create({
+    el: "#colorPicker",
+    theme: "nano",
+    swatches: [
+        'rgba(244, 67, 54, 1)',
+        'rgba(233, 30, 99, 1)',
+        'rgba(156, 39, 176, 1)',
+        'rgba(103, 58, 183, 1)',
+        'rgba(63, 81, 181, 1)',
+        'rgba(33, 150, 243, 1)',
+        'rgba(3, 169, 244, 1)',
+        'rgba(0, 188, 212, 1)',
+        'rgba(0, 150, 136, 1)',
+        'rgba(76, 175, 80, 1)',
+        'rgba(139, 195, 74, 1)',
+        'rgba(205, 220, 57, 1)',
+        'rgba(255, 235, 59, 1)',
+        'rgba(255, 193, 7, 1)'
+    ],
+    components: {
+        preview: true,
+        opacity: true,
+        hue: true,
+        // Input / output Options
+        interaction: {
+            hex: true,
+            rgba: false,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: true,
+            clear: false,
+            save: false
+        }
+    }
+});
+
+pickr.on("change", function (e) {
+    $(".pcr-button").css("--pcr-color", e.toRGBA().toString());
+    brushColor = e.toHEXA().toString();
+    canvas.freeDrawingBrush.color = brushColor;
+});
